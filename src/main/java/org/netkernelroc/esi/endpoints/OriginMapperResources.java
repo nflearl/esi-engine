38d86f5b661eaea53cb82de88273f74502a062ff
Earl Nolan <earl.nolan@nfl.com>
2014-03-31 13:39:13 -0700

    Build the OMR and wired it in.  Need to polish up the wiring a bit, wanted to checkpoint first.

---
+++ src/main/java/org/netkernelroc/esi/endpoints/OriginMapperResources.java
@@ -0,0 +1,32 @@
+package org.netkernelroc.esi.endpoints;
+
+import org.netkernel.layer0.nkf.INKFRequestContext;
+import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class provides the mapping from a host name to where the physical html files are located
+ * (whether on the file system or perhaps on a remote origin server).
+ */
+public class OriginMapperResources extends StandardAccessorImpl {
+
+    private static Map<String, String> hostNameMapping = new HashMap<String, String>(89);
+
+    // As an example.  One can have many different hosts mapping to different source locations.
+    static {
+        hostNameMapping.put("localhost", "file:/origin");
+    }
+
+    public void onSource(INKFRequestContext context) throws Exception
+    {
+        String host = context.getThisRequest().getArgumentValue("host");
+        // Happy case
+        if (hostNameMapping.containsKey(host))
+            context.createResponseFrom(hostNameMapping.get(host));
+        else
+            throw new UnsupportedOperationException("Oh no!, no host found");
+    }
+}
