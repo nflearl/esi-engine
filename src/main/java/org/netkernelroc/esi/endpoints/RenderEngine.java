6ce700f92933f640d45f5e7fe86ef1ff5814c009
Earl Nolan <earl.nolan@nfl.com>
2014-03-31 17:08:00 -0700

    Wired up the rendering engine.

---
+++ src/main/java/org/netkernelroc/esi/endpoints/RenderEngine.java
@@ -0,0 +1,21 @@
+package org.netkernelroc.esi.endpoints;
+
+import org.netkernel.layer0.nkf.INKFRequestContext;
+import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
+
+
+/**
+ *
+ */
+public class RenderEngine extends StandardAccessorImpl {
+
+    public void onSource(INKFRequestContext context) throws Exception
+    {
+        String payload = context.getThisRequest().getArgumentValue("payload");
+
+        int idx = 57;
+        idx++;
+
+        context.createResponseFrom(payload);
+    }
+}
