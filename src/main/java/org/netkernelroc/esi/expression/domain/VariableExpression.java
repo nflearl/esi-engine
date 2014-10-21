b32dc44822ac21c10a673f4f62cf8bc058c07a21
Earl Nolan <earl.nolan@nfl.com>
2014-10-20 17:45:59 -0700

    Got the other tests working for the false case.

--- src/main/java/org/netkernelroc/esi/expression/domain/VariableExpression.java
+++ src/main/java/org/netkernelroc/esi/expression/domain/VariableExpression.java
@@ -2,6 +2,9 @@ package org.netkernelroc.esi.expression.domain;
 
 import org.netkernel.layer0.representation.IHDSNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VariableExpression extends VariableOrLiteralExpression {
 
     public static final String NAME = "variableExpr";
@@ -11,6 +14,25 @@ public class VariableExpression extends VariableOrLiteralExpression {
 
     public VariableExpression(ExpressionBuilder builder) {
         super(builder);
+    }
+
+    @Override
+    boolean evaluateManyChildren(IHDSNode[] children) {
+        List<IHDSNode> remainders = new ArrayList<IHDSNode>(children.length);
+        // Throw out parens and try again.
+        for (IHDSNode child : children) {
+            if (child.getName().contains("PAREN"))
+                continue;
+
+            remainders.add(child);
+        }
+
+        if (remainders.size() != 1)
+            throw new UnsupportedOperationException("Don't know what to do with remainders of size: " + remainders.size());
+
+        IHDSNode child = remainders.get(0);
+        ESIExpression expr = getEb().build(child);
+        return expr.evaluate(child.getChildren());
     }
 
     @Override
